bf55c19d66a478fdd106c2b9693c376772696617
saiful islam <saiful01636758237@gmail.com>
2021-08-02 12:23:02 +0600

    form validation done

--- app/support/Database.php
+++ app/support/Database.php
@@ -1,14 +1,33 @@
 <?php 
 
 	namespace App\support;
+	use mysqli;
 
 
 	/**
 	 * Database connection
 	 */
-	class Database
+	abstract class Database
 	{
-		
+
+		/**
+		 * server related property
+		 */
+		private $host = "localhost";
+		private $user = "root";
+		private $pass = "";
+		private $db = "obj_crud";
+
+
+		private $connection;
+
+		/**
+		 * Database connection setup
+		 */
+		private function connection()
+		{
+			return $this -> connection = new mysqli($this -> host, $this -> user, $this -> pass, $this -> db);
+		}
 	}
 	
 	
--- index.php
+++ index.php
@@ -9,33 +9,75 @@
 	<link rel="stylesheet" href="assets/css/responsive.css">
 </head>
 <body>
+
+	<?php 
+
+	/**
+	* student form data manage
+	*/	
+
+	if (isset($_POST['submit'])) {
+		//data send
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$cell = $_POST['cell' ];
+		 
+		//file manage
+		$img = $_FILES['photo'];
+
+
+		if (empty($name) || empty($email) || empty($cell) ) {
+			 $mess = "<p class='alert alert-danger'>All Filds Are required ! <button class='close' data-dismiss='alert'>&times;</button></p>";
+		}else if(!filter_var($email, FILTER_VALIDATE_EMAIL) ) {
+				$mess = "<p class='alert alert-info'> Invalid Email Formate ! <button class='close' data-dismiss='alert'>&times;</button></p>";
+		}else{
+
+		}
+
+
+	}
+
+
+
+
+
+
+
+
+	 ?>
 	
-	
+
 
 	<div class="wrap ">
 		<a class="btn btn-primary btn-sm" href="data.php">All Data</a>
 		<div class="card shadow">
 			<div class="card-body">
 				<h2>Sign Up</h2>
-				<form action="">
+			<?php 
+
+				if (isset($mess)) {
+					echo $mess;
+				}
+			 ?>
+				<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype= "multipart/form-data">
 					<div class="form-group">
 						<label for="">Name</label>
-						<input class="form-control" type="text">
+						<input name="name" class="form-control" type="text">
 					</div>
 					<div class="form-group">
 						<label for="">Email</label>
-						<input class="form-control" type="text">
+						<input name="email" class="form-control" type="text">
 					</div>
 					<div class="form-group">
 						<label for="">Cell</label>
-						<input class="form-control" type="text">
+						<input name="cell" class="form-control" type="text">
 					</div>
 					<div class="form-group">
-						<label for="">Username</label>
-						<input class="form-control" type="text">
+						<label for="">Photo</label>
+						<input name="img" class="form-control" type="file">
 					</div>
 					<div class="form-group">
-						<input class="btn btn-primary" type="submit" value="Sign Up">
+						<input name="submit" class="btn btn-primary" type="submit" value="Sign Up">
 					</div>
 				</form>
 			</div>
